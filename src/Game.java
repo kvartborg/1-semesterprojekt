8c6ab901aa890c1916a9b619f3eb6f7907c82bf4
kvartborg <hello@kvartborg.net>
2017-09-22 11:55:12 +0200

    initial commit

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("You won!");
+    }
+
+}
