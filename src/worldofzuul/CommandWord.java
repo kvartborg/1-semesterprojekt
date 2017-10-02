a0290fa8d9b8d308b371d9d9ae6642b1be65676d
kvartborg <hello@kvartborg.net>
2017-10-02 11:13:28 +0200

    Add javadoc to CommandWord class

--- src/worldofzuul/CommandWord.java
+++ src/worldofzuul/CommandWord.java
@@ -5,14 +5,29 @@ package worldofzuul;
  * @version 2006.03.30
  */
 public enum CommandWord {
+    /**
+     * Available commands
+     */
     GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
 
+    /**
+     * The entered command
+     */
     private String commandString;
 
+    /**
+     * Create a new instance of a CommandWord
+     * @param commandString the commandString
+     * @return a new instance of the CommandWord
+     */
     CommandWord(String commandString) {
         this.commandString = commandString;
     }
 
+    /**
+     * If the CommandWord instance is used in a string context
+     * @return the commandString
+     */
     public String toString() {
         return commandString;
     }
