7f8f9e68f960b9c93a1e8a266e118c4cbf0b2bb4
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2017-10-02 12:52:03 +0200

    Added character, trump and player class (#8)
    * Added character, trump and player class
    
    * Added docblocks
    
    * Added docblock
    
    * Simplified the trump class
    
    * Player class extends Character

---
+++ src/worldofzuul/Character.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author Kasper
+ */
+public class Character {
+    /**
+     * The name of the character
+     */
+    private String name;
+    
+    /**
+     * A constructor for a character
+     * @param name The name of the character
+     */
+    public Character(String name){
+        this.name = name;
+    }
+    /**
+     * A no arguments constructor
+     */
+    Character(){
+        
+    }
+    
+}
