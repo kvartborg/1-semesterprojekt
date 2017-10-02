4c52460b460a9530a2158fafe0020d417acbbdaa
AndersBensen <32263695+AndersBensen@users.noreply.github.com>
2017-10-02 11:54:38 +0200

    add javadoc for game class (#5)
    * add javadoc for game class
    
    * removed unnecessary docblock
    
    * add dockblock for attributes in javadoc game
    
    * add docblock in javadoc game
    
    * edited docblock in javadoc game

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -4,15 +4,38 @@ package worldofzuul;
  * @author  Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
-public class Game {
-    private Parser parser;
-    private Room currentRoom;
 
+public class Game {
+    /** 
+     * parser attribute, an instance from the Parser class. 
+     */
+    private Parser parser;
+    /**
+     * currentRoom attribute, an instance from the Room class. 
+     * 
+     * The currentRoom variable is what room the player is in at the moment
+     * in game. 
+     */
+    private Room currentRoom;
+    
     public Game() {
+        /**
+         * This constructor method creates rooms that can be accessed in game.
+         */
         createRooms();
         parser = new Parser();
     }
-
+   /** 
+    * This method creates rooms, sets their exits and what room you start in. 
+    * 
+    * The method is called createRooms, and has no parameters. The first thing 
+    * it does is define what rooms you want in the game. Thereafter the code
+    * equals the room name and prints a string that comes up when you enter
+    * a new room. After this you set exits for each room and give what
+    * room you can go to from there. The last line sets the room you
+    * start in. 
+    * 
+    */ 
     private void createRooms() {
         Room outside, theatre, pub, lab, office;
 
@@ -37,7 +60,15 @@ public class Game {
 
         currentRoom = outside;
     }
-
+    /** 
+    * This method called play is what decides when the game is done.
+    * 
+    * The method starts with the printWelcome when the game is started, which 
+    * is printed in the method after. The code then sets a boolean that sets
+    * the finished to false. After a while loop with the condition not finished
+    * is made that checks what command the user has used. When the game is finished
+    * it jumps out the while loop and prints a string. 
+    */
     public void play() {
         printWelcome();
 
@@ -48,7 +79,13 @@ public class Game {
         }
         System.out.println("Thank you for playing.  Good bye.");
     }
-
+    /**
+    * This method prints strings when the game is started. 
+    * 
+    * The game first prints several strings with a welcoming message. It also
+    * gives a command that you can use to get help with the game. In the final
+    * line it also prints a description of the room you start in. 
+    */
     private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
@@ -57,7 +94,17 @@ public class Game {
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
     }
-
+    /**
+    * This method process the different comamnds and decide what they do. 
+    * 
+    * The method is a multi if /else depending on what command the user 
+    * has chosen to use. It gives different options on what will happen
+    * if the user uses an unknown command or if the user uses a known command.
+    * 
+    * 
+    * @param command
+    * @return boolean (wantToQuit, false or true)
+    */
     private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
@@ -78,7 +125,12 @@ public class Game {
 
         return wantToQuit;
     }
-
+    /**
+    * This method prints a strings to help the user. 
+    * 
+    * The method prints strings with what the game is about and prints 
+    * what different commands the user has if the user uses the command help. 
+    */
     private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
@@ -86,6 +138,15 @@ public class Game {
         System.out.println("Your command words are:");
         parser.showCommands();
     }
+    /**
+     * This method checks if the command has a second word. 
+     * 
+     * The method firstly checks if the "go" command has a second word or not.
+     * if it doesnt then it tells the user to use a second word. 
+     * It also changes the currentroom to the the next room the user wants
+     * to access. 
+     * @param command 
+     */
 
     private void goRoom(Command command) {
         if(!command.hasSecondWord()) {
@@ -104,7 +165,17 @@ public class Game {
             System.out.println(currentRoom.getLongDescription());
         }
     }
-
+    /**
+    * This method quits the game.
+    * 
+    * The method decides what happens when the user uses the quit command. 
+    * It first checks if the uses posted a second word when quitting, returns
+    * false if it has. If the user doesnt have a second word after quit, then
+    * its true and therefore quits the game. 
+    * 
+    * @param command
+    * @return boolean (false if second word, true if not) 
+    */
     private boolean quit(Command command) {
         if(command.hasSecondWord()) {
             System.out.println("Quit what?");
