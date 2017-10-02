4362cd19e9951ad68134ce4897e59577306d0364
AndersBensen <32263695+AndersBensen@users.noreply.github.com>
2017-10-02 11:46:06 +0200

    Add javadoc for bootstrap file (#6)

--- src/Bootstrap.java
+++ src/Bootstrap.java
@@ -2,7 +2,12 @@ import worldofzuul.Game;
 
 public class Bootstrap {
 
-    /**
+    /** 
+     * This method starts the game. 
+     * 
+     * It is the main method for the whole project and creates an object
+     * game from the class Game. After that it initializes the games beginning.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
