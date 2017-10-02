2599c392409f72c33d559b7ffe679092c5217857
muggell <32263698+muggell@users.noreply.github.com>
2017-10-02 11:48:28 +0200

    Add javadoc commandwords (#3)
    * Add javadoc commandwords
    
    * Fixed spaces/tabs
    
    * Fixed space
    
    * Fixed docblock for attribute

--- src/worldofzuul/CommandWords.java
+++ src/worldofzuul/CommandWords.java
@@ -7,18 +7,33 @@ import java.util.HashMap;
  * @version 2006.03.30
  */
 
-public class CommandWords {
-    private HashMap<String, CommandWord> validCommands;
 
+public class CommandWords {
+    /**
+     * A list of valid commands
+     */
+    private HashMap<String, CommandWord> validCommands;
+    
+    /**
+     * Creating an instance of CommandWords
+     * Creating an empty list 
+     * Validating if the commandword is known
+     * Storing if its valid
+     */
     public CommandWords() {
         validCommands = new HashMap<String, CommandWord>();
-        for(CommandWord command : CommandWord.values()) {
-            if(command != CommandWord.UNKNOWN) {
+        for(CommandWord command : CommandWord.values()) { 
+            if(command != CommandWord.UNKNOWN) {   
                 validCommands.put(command.toString(), command);
             }
         }
     }
-
+    
+    /**
+     * Creating a getter method for CommandWord
+     * @param commandWord
+     * @return if command != null return command otherwise return unknown 
+     */
     public CommandWord getCommandWord(String commandWord) {
         CommandWord command = validCommands.get(commandWord);
         if(command != null) {
@@ -27,11 +42,19 @@ public class CommandWords {
             return CommandWord.UNKNOWN;
         }
     }
-
+    
+    /**
+     * Creating a boolean expression 
+     * @param aString
+     * @return true if valid commands contains the given string
+     */
     public boolean isCommand(String aString) {
         return validCommands.containsKey(aString);
     }
-
+    
+    /**
+     * Printing all available commands
+     */
     public void showAll() {
         for(String command : validCommands.keySet()) {
             System.out.print(command + "  ");
