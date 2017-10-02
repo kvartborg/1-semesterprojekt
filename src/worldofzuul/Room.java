7f06b0afd728c19ea04dd3fe8ba34f194a51447c
Kasper Møller Nielsen <kaspermn33@gmail.com>
2017-10-02 10:58:14 +0200

    Added comments for methods, constructors and attributes

--- src/worldofzuul/Room.java
+++ src/worldofzuul/Room.java
@@ -11,22 +11,48 @@ import java.util.Iterator;
  */
 public class Room {
 
+    /**
+     * The String "description" is used when describing the room generated with the constructor
+     * The HashMap is a list of existing rooms
+     */
     private String description;
     private HashMap<String, Room> exits;
 
+    /**
+     * A constructor for new rooms
+     * @param description 
+     * Description is the description of the new room
+     */
     public Room(String description) {
         this.description = description;
         exits = new HashMap<String, Room>();
     }
-
+    
+    /**
+     * The method sets the exits for the current room
+     * @param direction 
+     * Direction is the direction of the other rooms
+     * @param neighbor 
+     * Neighbor is the name of the other rooms
+     */
     public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
-
+    
+    /**
+     * The method gets the short description of the room
+     * @return 
+     * Returns description (only the name returns)
+     */
     public String getShortDescription() {
         return description;
     }
 
+    /**
+     * The method gets the long description of the room
+     * @return 
+     * Returns "You are ", the description and the exits of the current room
+     */
     public String getLongDescription() {
         return "You are " + description + ".\n" + getExitString();
     }
@@ -39,7 +65,14 @@ public class Room {
         }
         return returnString;
     }
-
+    
+    /**
+     * The method gets the room in the direction described
+     * @param direction
+     * Direction is the decider of what exit is used
+     * @return 
+     * Returns the room in the decided direction
+     */    
     public Room getExit(String direction) {
         return exits.get(direction);
     }
