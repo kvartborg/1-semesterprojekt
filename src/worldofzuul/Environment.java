7ea4dc5e571a8843ae4938bbdedf8a189b27931b
kvartborg <hello@kvartborg.net>
2017-10-09 12:40:59 +0200

    Refactor game class to use the new environment and chareters

--- src/worldofzuul/Environment.java
+++ src/worldofzuul/Environment.java
@@ -15,60 +15,67 @@ public class Environment {
     /**
      * HashMap attribute for rooms and their names
      */
-    private HashMap<String, Room> rooms;
-    
+    private HashMap<String, Room> rooms = new HashMap<>();
+
     /**
      * This method is a getter method
      * @param name
      * @return returns name of room
      */
     public Room getRoom(String name) {
-        return rooms.get(name);
+        return this.rooms.get(name);
     }
-    
+
+    /**
+     * Create a new instance of Environment
+     */
+    public Environment() {
+        this.createRooms();
+    }
+
     /**
      * This method create the different rooms in our game.
-     * 
+     *
      * We create the rooms in our game and sets an exit for each room.
      * We put our room into a HashMap. The method returns the rooms
      * into our HashMap.
      */
     private void createRooms() {
         Room ovalOffice, lobby1, lobby2, kitchen, diningRoom, cleaningRoom, pressBriefingRoom, secretServiceRoom;
-        
+
         ovalOffice = new Room("in the oval office");
-        lobby1 = new Room("in the first lobby"); 
+        lobby1 = new Room("in the first lobby");
         lobby2 = new Room("in the second lobby");
         kitchen = new Room("in the kitchen");
         diningRoom = new Room("in the dining room");
         cleaningRoom = new Room("in the cleaning room");
         pressBriefingRoom = new Room("in the press briefing room");
-        secretServiceRoom = new Room("in the secret service room"); 
-        
+        secretServiceRoom = new Room("in the secret service room");
+
         ovalOffice.setExit("north", lobby1);
-        
+
         lobby1.setExit("north", lobby2);
         lobby1.setExit("east", secretServiceRoom);
         lobby1.setExit("south", ovalOffice);
-        lobby1.setExit("west", diningRoom); 
-        
+        lobby1.setExit("west", diningRoom);
+
         diningRoom.setExit("north", kitchen);
-        diningRoom.setExit("east", lobby1); 
-        
-        secretServiceRoom.setExit("west", lobby1); 
-        
+        diningRoom.setExit("east", lobby1);
+
+        secretServiceRoom.setExit("west", lobby1);
+
         lobby2.setExit("north", cleaningRoom);
         lobby2.setExit("east", pressBriefingRoom);
         lobby2.setExit("south", lobby1);
-        lobby2.setExit("west", kitchen); 
-        
+        lobby2.setExit("west", kitchen);
+
         kitchen.setExit("west", lobby2);
         kitchen.setExit("south", diningRoom);
-        
-        pressBriefingRoom.setExit("west", lobby2); 
-        
-        cleaningRoom.setExit("south", lobby2); 
-        
+
+        pressBriefingRoom.setExit("west", lobby2);
+
+        cleaningRoom.setExit("south", lobby2);
+
         rooms.put("Oval office", ovalOffice);
         rooms.put("Lobby1", lobby1);
         rooms.put("Lobby2", lobby2);
