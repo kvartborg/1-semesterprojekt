5776f0fbc78b3d5c62926a1fffeb499f2849f79f
RasmusAlbrektsen <32238291+RasmusAlbrektsen@users.noreply.github.com>
2017-10-09 11:35:32 +0200

    add-character (#17)
    * add-character
    
    Added the methods and attributes to characters, read in the diagram.
    
    * Fixed for comments
    
    add-character with fixes.
    
    * Fixed with no args
    
    removed no arg constructor in Character.
    
    * Removed goRoom in Game
    
    * Simplified the isInventoryFull

--- src/worldofzuul/Character.java
+++ src/worldofzuul/Character.java
@@ -10,6 +10,17 @@ package worldofzuul;
  * @author Kasper
  */
 public class Character {
+    
+    /**
+     * currentRoom attribute, an instance from the Room class. 
+     * 
+     * The currentRoom variable is what room the player is in at the moment
+     * in game. 
+     */
+    private Room currentRoom;
+    
+    
+    
     /**
      * The name of the character
      */
@@ -22,11 +33,32 @@ public class Character {
     public Character(String name){
         this.name = name;
     }
-    /**
-     * A no arguments constructor
-     */
-    Character(){
-        
-    }
     
+    
+     /**
+     * This method checks if the command has a second word. 
+     * 
+     * The method firstly checks if the "go" command has a second word or not.
+     * if it doesnt then it tells the user to use a second word. 
+     * It also changes the currentroom to the the next room the user wants
+     * to access. 
+     * @param command 
+     */
+      private void goRoom(Command command) {
+        if(!command.hasSecondWord()) {
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        Room nextRoom = currentRoom.getExit(direction);
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        } else {
+            currentRoom = nextRoom;
+            System.out.println(currentRoom.getLongDescription());
+        }
+    }
 }
