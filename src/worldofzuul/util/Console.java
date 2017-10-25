3857bec0f53d97ccb45a0cb359ee911cf0aafb8d
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2017-10-25 11:33:43 +0200

    Add console class for printing messages with ease (#62)
    * Add console class for printing messages with ease
    
    * Fixed typo

---
+++ src/worldofzuul/util/Console.java
@@ -0,0 +1,9 @@
+package worldofzuul.util;
+
+public class Console {
+    public static void print (String ...messages) {
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
+}
