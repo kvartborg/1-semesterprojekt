a9293f46831abb3a523041a151dbd50b892ae29f
RasmusAlbrektsen <32238291+RasmusAlbrektsen@users.noreply.github.com>
2017-10-25 11:46:09 +0200

    fixed javadoc for game

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -192,6 +192,11 @@ public class Game {
          steps++;
          Console.print(steps + " step(s) taken");
     }
+    
+    /**
+     * Method to randomize the movement of the "Trump" character. 
+     * The method uses "Math" to choose a random direction.
+     */
     private void randomizeTrump() {
 
          String[] possibleDirections = {"east", "west", "north", "south"};
