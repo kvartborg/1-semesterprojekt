777452b46892f754d8ac8e28fe14fffce24280b0
AndersBensen <32263695+AndersBensen@users.noreply.github.com>
2017-11-03 11:55:06 +0100

    Added time bonus (#82)
    * Added time bonus
    
    Kasper and Anders added a time bonus for doing a specific thing in the game.
    
    * remade console print
    
    Kasper and Anders remade the console print

--- src/maga/Game.java
+++ src/maga/Game.java
@@ -53,6 +53,11 @@ public class Game {
     private Long startTime = System.currentTimeMillis() / 1000L;
     
     /**
+     * Bonus time
+     */
+    private Long bonusTime = 0L; 
+    
+    /**
      * Integer that sets the point the player starts with.
      */
     private int points = 5000;
@@ -177,6 +182,7 @@ public class Game {
 
             case CALLTRUMP:
                 points += trump.findSteak(environment.getRoom("Cleaning room"), player);
+                bonusTime += points/333;  
                 break;
             
         }
@@ -289,15 +295,18 @@ public class Game {
         }
         long endTime = System.currentTimeMillis() / 1000L;
         long elapsedTime = endTime - startTime;
-        long finalScore = points - (elapsedTime * steps);
+        long finalScore = points - ((elapsedTime - bonusTime) * steps);
         Console.print(
             "",
             "Congratulations, you won the game!",
             "",
             "---------------------------------------",
             "You made it in " + steps + " steps, in " + elapsedTime + " seconds!",
+            bonusTime > 0 ? "You received a time bonus: " + bonusTime + " seconds, for calling Trump!" : null,
+            bonusTime > 0 ? "Your time after receiving the bonus is: " + (elapsedTime-bonusTime) + " seconds!" : null,
             "You scored: " + finalScore,
             "---------------------------------------"
+             
         );
         return true;
     }
--- src/maga/util/Console.java
+++ src/maga/util/Console.java
@@ -3,6 +3,9 @@ package maga.util;
 public class Console {
     public static void print (String ...messages) {
         for (String message : messages) {
+            if (message == null) {
+                continue; 
+            }
             System.out.println(message);
         }
     }
