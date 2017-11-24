473403982f62bf50b9db6b58d8d5d86ab8be85c5
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2017-11-24 13:46:59 +0100

    implement call trump (#112)
    Coded by Kasper and Viktoria

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -126,8 +126,11 @@ public class GUI extends Application {
             break;
            
             case I:
-               this.loadInventoryController();
+                this.loadInventoryController();
             break;
+            
+            case C:
+                game.command("call","");
        }
       
        this.gameController.updateGameState();
